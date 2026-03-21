cecf54f0c280d32f1724be8da1f2edf42da23042
p1ll3chan <am.en.u4mee23002@am.students.amrita.edu>
2026-03-21 10:16:57 +0530

    Practice update: Saturday 21 March 2026 10:16:57 AM IST

---
+++ ICPC_Assult_U_Sheeet/T_Sort_Numbers.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define all(x) (x).begin(), (x).end()
+#define endl "\n"
+#define sz(x) (int)((x).size())
+#define f first
+#define s second
+#define pb push_back
+#define vi vector<int>
+#define rall(c) c.rbegin(),c.rend()
+#define vii vector<vector<int>>
+#define pii pair<int, int>
+#define fastio() ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define YES cout << "Yes" << endl
+#define NO cout << "No" << endl
+#define YESNO(x) cout << ((x) ? "YES" : "NO") << endl
+#define debug(x) cerr << #x << " = " << (x) << endl;
+#define debugArr(arr) for (auto v : arr) cerr << v << " "; cerr << endl;
+#define readVec(v, n) for (int i = 0; i < n; ++i) cin >> v[i];
+#define readMatrix(mat, n, m) for (int i = 0; i < n; ++i) for (int j = 0; j < m; ++j) cin >> mat[i][j];
+#define printVec(v) for (auto x : v) cout << x << " "; cout << endl;
+#define printMatrix(mat) for (auto r : mat) { printVec(r); }
+#define fp(i, a, b) for (int i = (a); i < (b); ++i)
+
+void solve() {
+    int a,b,c;cin>>a>>b>>c;
+    vector<int> v={a,b,c};
+    sort(all(v));
+    cout<<v[0]<<endl;
+
+    cout<<v[1]<<endl;
+    cout<<v[2]<<endl;
+    cout<<endl;
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<c<<endl;
+}
+
+void test() {
+    int t;
+    cin >> t;
+    while (t--) solve();
+}
+
+int32_t main() {
+    fastio();
+    // test();
+    solve();
+    return 0;
+}
