cd80de44f0d1bb2e3a198a0045d5535434544dde
p1ll3chan <am.en.u4mee23002@am.students.amrita.edu>
2026-03-26 01:37:05 +0530

    Practice update: Thursday 26 March 2026 01:37:05 AM IST

---
+++ ICPC_Assult_U_Sheeet/C_Even_Odd_Positive_and_Negative.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define all(x) (x).begin(), (x).end()
+#define endl "\n"
+#define sz(x) (int)((x).size())
+#define f first
+#define s second
+#define pb push_back
+#define vi vector<int>
+#define rall(c) c.rbegin(),c.rend()
+#define vii vector<vector<int>>
+#define pii pair<int, int>
+#define fastio() ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define YES cout << "Yes" << endl
+#define NO cout << "No" << endl
+#define YESNO(x) cout << ((x) ? "YES" : "NO") << endl
+#define debug(x) cerr << #x << " = " << (x) << endl;
+#define debugArr(arr) for (auto v : arr) cerr << v << " "; cerr << endl;
+#define readVec(v, n) for (int i = 0; i < n; ++i) cin >> v[i];
+#define readMatrix(mat, n, m) for (int i = 0; i < n; ++i) for (int j = 0; j < m; ++j) cin >> mat[i][j];
+#define printVec(v) for (auto x : v) cout << x << " "; cout << endl;
+#define printMatrix(mat) for (auto r : mat) { printVec(r); }
+#define fp(i, a, b) for (int i = (a); i < (b); ++i)
+
+void solve() {
+    int n;
+    cin >> n;
+    vi v(n);
+    readVec(v, n);
+    int even=0,odd=0,pos=0,neg=0;
+    for (int i = 0; i < n; i++)
+    {
+        if(v[i]<0) neg++;
+        if(v[i]>0) pos++;
+        if(v[i]%2==0) even++;
+        if(v[i]%2!=0) odd++;
+    }
+    cout<<"Even: "<<even<<endl;
+    cout<<"Odd: "<<odd<<endl;
+    cout<<"Positive: "<<pos<<endl;
+    cout<<"Negative: "<<neg<<endl;
+    
+    // Your logic here
+}
+
+void test() {
+    int t;
+    cin >> t;
+    while (t--) solve();
+}
+
+int32_t main() {
+    fastio();
+    // test();
+    solve();
+    return 0;
+}
---
+++ ICPC_Assult_U_Sheeet/E_Max.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define all(x) (x).begin(), (x).end()
+#define endl "\n"
+#define sz(x) (int)((x).size())
+#define f first
+#define s second
+#define pb push_back
+#define vi vector<int>
+#define rall(c) c.rbegin(),c.rend()
+#define vii vector<vector<int>>
+#define pii pair<int, int>
+#define fastio() ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define YES cout << "Yes" << endl
+#define NO cout << "No" << endl
+#define YESNO(x) cout << ((x) ? "YES" : "NO") << endl
+#define debug(x) cerr << #x << " = " << (x) << endl;
+#define debugArr(arr) for (auto v : arr) cerr << v << " "; cerr << endl;
+#define readVec(v, n) for (int i = 0; i < n; ++i) cin >> v[i];
+#define readMatrix(mat, n, m) for (int i = 0; i < n; ++i) for (int j = 0; j < m; ++j) cin >> mat[i][j];
+#define printVec(v) for (auto x : v) cout << x << " "; cout << endl;
+#define printMatrix(mat) for (auto r : mat) { printVec(r); }
+#define fp(i, a, b) for (int i = (a); i < (b); ++i)
+
+void solve() {
+    int n;
+    cin >> n;
+    vi v(n);
+    readVec(v, n);
+    int max_val=0;
+    for (int i = 0; i < n; i++)
+    {
+        if(max_val<v[i]) max_val=v[i];
+    }
+    cout<<max_val<<endl;
+    // Your logic here
+}
+
+void test() {
+    int t;
+    cin >> t;
+    while (t--) solve();
+}
+
+int32_t main() {
+    fastio();
+    // test();
+    solve();
+    return 0;
+}
