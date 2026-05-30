dddefe81b215486db39c8497badc987137f2e1c6
p1ll3chan <am.en.u4mee23002@am.students.amrita.edu>
2026-05-30 19:55:56 +0530

    Practice update: Saturday 30 May 2026 07:55:56 PM IST

--- Atcoder/ABC459/Adjacent_Distinct_String.cpp
+++ Atcoder/ABC459/Adjacent_Distinct_String.cpp
@@ -3,64 +3,76 @@ using namespace std;
 #pragma GCC optimize("Ofast")
 
 #define int long long
-#define fastio() ios::sync_with_stdio(0); cin.tie(0)
+#define all(x) (x).begin(), (x).end()
+#define endl "\n"
+#define sz(x) (int)((x).size())
+#define f first
+#define s second
+#define pb push_back
+#define vi vector<int>
+#define rall(c) c.rbegin(),c.rend()
+#define vii vector<vector<int>>
+#define pii pair<int, int>
+#define fastio() ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define YES cout << "Yes" << endl
+#define NO cout << "No" << endl
+#define YESNO(x) cout << ((x) ? "YES" : "NO") << endl
+#define debug(x) cerr << #x << " = " << (x) << endl;
+#define debugArr(arr) for (auto v : arr) cerr << v << " "; cerr << endl;
+#define readVec(v, n) for (int i = 0; i < n; ++i) cin >> v[i];
+#define readMatrix(mat, n, m) for (int i = 0; i < n; ++i) for (int j = 0; j < m; ++j) cin >> mat[i][j];
+#define printVec(v) for (auto x : v) cout << x << " "; cout << endl;
+#define printMatrix(mat) for (auto r : mat) { printVec(r); }
+#define fp(i, a, b) for (int i = (a); i < (b); ++i)
+
+/*
+----
+Problem:
+
+My Intuition:
+
+Approach 1:
+
+Why Failed:
+
+Approach 2:
+
+Example Process:
+
+Final Learning:
+
+------
+*/
 
 void solve() {
-    string s;
-    cin >> s;
-
-    int n = s.size();
-
-    vector<int> freq(26, 0);
-
-    for(char c : s)
-        freq[c - 'a']++;
-
-    int mx = *max_element(freq.begin(), freq.end());
-
-    if(mx > (n + 1) / 2) {
-        cout << "NO\n";
-        return;
+    string s;cin>>s;
+    bool ok=false;
+    do{
+        for (int i = 0; i < s.size()-1; i++)
+        {
+            if(s[i]!=s[i+1]) ok=true;
+        }
+        if(ok){
+            YES;
+            cout<<s<<endl;
+        }else{
+            NO;
+        }
+        
     }
+    while(next_permutation(all(s)));
+    // Your logic here
+}
 
-    priority_queue<pair<int,char>> pq;
-
-    for(int i = 0; i < 26; i++) {
-        if(freq[i])
-            pq.push({freq[i], char('a' + i)});
-    }
-
-    string ans = "";
-
-    pair<int,char> prev = {0, '#'};
-
-    while(!pq.empty()) {
-
-        auto [cnt, ch] = pq.top();
-        pq.pop();
-
-        ans += ch;
-        cnt--;
-
-        // push previous back
-        if(prev.first > 0)
-            pq.push(prev);
-
-        prev = {cnt, ch};
-    }
-
-    cout << "YES\n";
-    cout << ans << '\n';
+void test() {
+    int t;
+    cin >> t;
+    while (t--) solve();
 }
 
 int32_t main() {
     fastio();
-
-    int t;
-    cin >> t;
-
-    while(t--)
-        solve();
-
+    test();
+    //solve();
     return 0;
 }
